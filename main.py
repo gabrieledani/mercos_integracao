0d45a2c80150c70a22357c7d458cd20b6ca3f611
Gabriele Dani <gabrieledani@gmail.com>
2023-05-31 17:57:24 -0300

    31-05

--- main.py
+++ main.py
@@ -56,10 +56,10 @@ def send_mail(filename,subject,body):
             smtpObj.login(login, password)
             smtpObj.sendmail(sender_email, receiver_email, msg.as_string())
             log_file.write(str(datetime.datetime.now())+'--success\n')
-            #print('success')
+            print('success')
             return 'success'
     except Exception as e:
-        #print(e)
+        print(e)
         log_file.write(str(datetime.datetime.now())+'--'+str(e)+'\n')
         return e
 
@@ -71,20 +71,20 @@ def read_mail():
     # *mark emails as seen on fetch, see mark_seen arg
     for msg in mailbox.fetch(AND(from_='vedafil', seen=False)):#,mark_seen=False):  
         subject = msg.subject
-        #print(subject)
+        print(subject)
         
         pedido = subject[subject.find('Nº')+3:subject.find('-')-1] 
-        #print(pedido)
+        print(pedido)
         
         body = msg.html
-        #print(body)
+        print(body)
 
         soup = BeautifulSoup(body, 'html.parser')
 
         link = soup.find_all('a')
         # URL from which pdfs to be downloaded
         pdf_file_url = link[1].get('href', [])
-        #print(link[1].get('href', []))
+        print(link[1].get('href', []))
         
         text = soup.find_all('div')
         content = text[2].text
@@ -92,7 +92,7 @@ def read_mail():
         content = content[:att]+'\n'+content[att:]
         if content.find('Hengst / Hengst Indústria de Filtros Ltda')>=0:
             content = ''
-        #print(content)
+        print(content)
 
         # Requests URL and get response object
         response = requests.get(pdf_file_url)
@@ -109,7 +109,7 @@ def read_mail():
         link = links[0]
         if ('.pdf' in link.get('href', [])):
             i += 1
-            #print("Downloading file: ", i)
+            print("Downloading file: ", i)
 
             # Get response object for link
             response = requests.get(link.get('href'))
@@ -119,7 +119,7 @@ def read_mail():
             pdf = open(os.path.join(dir_pdf,file), 'wb')
             pdf.write(response.content)
             pdf.close()
-            #print("File ", os.path.join(dir_pdf,pedido+".pdf"), " downloaded")
+            print("File ", os.path.join(dir_pdf,pedido+".pdf"), " downloaded")
 
             #Generate EDI from PDF
             error = 0
@@ -131,7 +131,7 @@ def read_mail():
                 error = 1
         
         if error == 0:
-            #print(edi_filename,subject,content)
+            print(edi_filename,subject,content)
             error = send_mail(edi_filename,subject,content)
             if error == 'success':
                 try:
